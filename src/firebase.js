7cd0ec377c0729530ad51176bce9eefb7a584014
hyunhaha <gusgk2658@naver.com>
2021-02-05 16:35:17 +0900

    hide firebase api keys

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/storage';
 var firebaseConfig = {
-  apiKey: "AIzaSyBhrHJLGb_yBTttGRCRtkRszWPLGB8UwEk",
-  authDomain: "chatting-app-381b1.firebaseapp.com",
-  projectId: "chatting-app-381b1",
-  storageBucket: "chatting-app-381b1.appspot.com",
-  messagingSenderId: "452624205649",
-  appId: "1:452624205649:web:f8315ad9a460a73b67b570",
-  measurementId: "G-RXEY0R7S3Y"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
